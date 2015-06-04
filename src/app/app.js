b43ea934bd1d81fbd9d90312b4efc56b8eb8fa4f
unknown <john.a.harding@ericsson.com>
2015-06-04 17:22:38 +0100

    Routing and views

--- src/app/app.js
+++ src/app/app.js
@@ -1,1 +1,15 @@
-angular.module("customersApp", []);
+angular.module("customersApp", ['ngRoute'])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                controller: 'customersController',
+                templateUrl: 'app/views/customers.html'
+            })
+            .when('/orders/:customerId', {
+                controller: 'ordersController',
+                templateUrl: 'app/views/orders.html'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
